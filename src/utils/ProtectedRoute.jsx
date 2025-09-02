ced5d43db9702a6f9d92290d11f15351329d7258
wdcs-tapanadeshara <tapan.adeshara@codezeros.com>
2025-09-02 15:12:22 +0530

    react to next

--- src/utils/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
- import { Outlet, Navigate } from 'react-router-dom';
-
-    const ProtectedRoute = () => {
-      const isAuthenticated = localStorage.getItem('token'); 
-            return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-    };
-
-    export default ProtectedRoute;
